0e6c81a97d6f38483ade8cd0e03c629381cb3cfa
jemssss16 <jamesgutierrezmarfil@gmail.com>
2024-10-04 19:12:54 +0800

    new

--- input.php
+++ input.php
@@ -3,9 +3,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Database connection
     $conn = new mysqli("localhost", "username", "password", "database");
 
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Database connection
+        $conn = new mysqli("localhost", "username", "password", "database");
     }
-}
 ?>
